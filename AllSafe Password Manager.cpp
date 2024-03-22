37ac611c985a7fc3f2c3770348054babb13cc5aa
Yousef Shaheen <115540431+YS2100344@users.noreply.github.com>
2024-03-22 22:56:13 +0200

    Trim Function AllSafe Password Manager.cpp

--- AllSafe Password Manager.cpp
+++ AllSafe Password Manager.cpp
@@ -20,7 +20,14 @@ string vigenereCipher(const string& text, const string& key, bool encrypt) {
     }
     return result;
 }
-
+string trim(const string& str) {
+    size_t first = str.find_first_not_of(' ');
+    if (string::npos == first) {
+        return str;
+    }
+    size_t last = str.find_last_not_of(' ');
+    return str.substr(first, (last - first + 1));
+}
 bool containsNumberAndSpecialChar(const string& str) {
     bool hasNumber = false, hasSpecial = false;
     for (char c : str) {
